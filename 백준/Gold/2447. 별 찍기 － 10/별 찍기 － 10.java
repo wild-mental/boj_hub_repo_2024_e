9b88f49a31bed9caa53f4049e7c32b0af3372a91
wild-mental <wildmental@knou.ac.kr>
2024-01-29 22:20:28 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 404 ms, Memory: 37608 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,12 +1,10 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Main {
     static int[][] unit = {
-            new int[] {1,1,1},
-            new int[] {1,0,1},
-            new int[] {1,1,1}
+            {1,1,1},
+            {1,0,1},
+            {1,1,1}
     };
     static int unitLv = 1;
     public static void main(String[] args) throws IOException {
@@ -35,9 +33,9 @@ public class Main {
 //        System.out.printf("shift:%d newSize:%d \n", shift, newSize);
         int[][] newUnit = new int[newSize][newSize];
         int[][] indexShifter = {
-            new int[] {0,0}, new int[] {0,1}, new int[] {0,2},
-            new int[] {1,0},                  new int[] {1,2},
-            new int[] {2,0}, new int[] {2,1}, new int[] {2,2},
+            {0,0},{0,1},{0,2},
+            {1,0},      {1,2},
+            {2,0},{2,1},{2,2},
         };
         // 기존 유닛 내부의 모든 좌표를 순회 하면서
         for (int i=0; i<unit.length; i++){
