591a6fc1733eff2adfec5c4d0ed0f297e9c92c5e
wild-mental <wildmental@knou.ac.kr>
2024-02-25 18:28:25 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,7 +1,7 @@
 import math
-input_val = input().split()
-print(int(input_val[0])+int(input_val[1]))
-print(int(input_val[0])-int(input_val[1]))
-print(int(input_val[0])*int(input_val[1]))
-print(math.floor(int(input_val[0])/int(input_val[1])))
-print(int(input_val[0])%int(input_val[1]))
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
